c1677e4b94ee60202f29c7945c6d2d9c570c82c6
Anze <peternelanze@gmail.com>
2026-06-19 21:43:11 +0200

    chore: integrate tailor

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const Reader = @import("Reader.zig");
+const Tailer = @import("Tailer.zig");
 
 test {
     _ = @import("Tailer.zig");
@@ -10,10 +10,10 @@ pub fn main(init: std.process.Init) !void {
     var write_buffer: [1024]u8 = undefined;
     var file_writer = std.Io.File.stdout().writer(init.io, &write_buffer);
 
-    var read_buffer: [64 * 1024]u8 = undefined;
-    const r = Reader.init(init.io, &file_writer.interface, .KEEP_OPEN);
+    var t = try Tailer.open(init.gpa, init.io, std.Io.Dir.cwd(), "logs/sample.log");
+    defer t.deinit();
 
-    r.read(std.Io.Dir.cwd(), "logs/sample.log", &read_buffer) catch {
+    t.follow(&file_writer.interface) catch {
         std.process.exit(1);
     };
 }
