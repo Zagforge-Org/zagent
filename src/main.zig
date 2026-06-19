639175182c3a75011776bb292715eb356c6e113b
Anze <peternelanze@gmail.com>
2026-06-19 23:55:51 +0200

    chore: register ring buffer

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,24 @@
 const std = @import("std");
 const Tailer = @import("Tailer.zig");
+const RingBuffer = @import("RingBuffer.zig");
 
 test {
     _ = @import("Tailer.zig");
     _ = @import("Reader.zig");
+    _ = @import("RingBuffer_test.zig");
 }
 
 pub fn main(init: std.process.Init) !void {
     var write_buffer: [1024]u8 = undefined;
     var file_writer = std.Io.File.stdout().writer(init.io, &write_buffer);
 
+    var ring = try RingBuffer.init(init.gpa, 1024);
+    defer ring.deinit();
+
     var t = try Tailer.open(init.gpa, init.io, std.Io.Dir.cwd(), "logs/app.log");
     defer t.deinit();
 
-    t.follow(&file_writer.interface) catch {
+    t.follow(&file_writer.interface, &ring) catch {
         std.process.exit(1);
     };
 }
